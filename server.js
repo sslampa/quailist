1d5fae002032c4c1b2bf409f62dc79270dd851ae
sslampa <sslampa@gmail.com>
2017-07-16 23:55:16 -0700

    Add mongoose connection functions
    Remove deprecation warning
    
    Syntax change

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
 const port = 3000
 
 const helloWorld = require('./controllers/hello-world')
+
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/quailist-development', { 
+    useMongoClient: true 
+  })
+  .then(() => console.log('Connection Successful'))
+  .catch((err) => console.error(err))
 
 app.get('/api/hello-world', helloWorld.helloWorld)
 app.get('/', helloWorld.getHello)
