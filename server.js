3fc2c942f917a36b74b42439288d474576c39581
sslampa <sslampa@gmail.com>
2017-07-25 23:09:38 -0700

    Add body-parser to server.js

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ mongoose.connect('mongodb://localhost/quailist-development', {
   .then(() => console.log('Connection Successful'))
   .catch((err) => console.error(err))
 
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
 app.get('/api/test', test.getTest)
 app.get('/', (req, res) => {
   res.send('Hello World!')
